c5d4364b6746f2642ebe19d7e731299612024102
ToasterXE <114070865+ToasterXE@users.noreply.github.com>
2023-11-02 15:20:34 +0100

    was losoos

--- main.js
+++ main.js
@@ -1,16 +1,26 @@
-let myButton = document.querySelector("button");
-let myHeading = document.querySelector("h1");
-function setUserName() {
-    const myName = prompt("Please enter your name.");
-    localStorage.setItem("name", myName);
-    myHeading.textContent = `Mozilla is cool, ${myName}`;
-  }
+let createButton = document.querySelector("create");
+
+function showlogin(){
+  document.getElementById("dtl").classList.toggle("show");
+}
+
+
+
+
+
+// let myButton = document.querySelector("button");
+// let myHeading = document.querySelector("h1");
+// function setUserName() {
+//     const myName = prompt("Please enter your name.");
+//     localStorage.setItem("name", myName);
+//     myHeading.textContent = `Mozilla is cool, ${myName}`;
+//   }
 
   
-  if (!localStorage.getItem("name")) {
-    setUserName();
-  } else {
-    const storedName = localStorage.getItem("name");
-    myHeading.textContent = `Mozilla is cool, ${storedName}`;
-  }
+//   if (!localStorage.getItem("name")) {
+//     setUserName();
+//   } else {
+//     const storedName = localStorage.getItem("name");
+//     myHeading.textContent = `Mozilla is cool, ${storedName}`;
+//   }
   
