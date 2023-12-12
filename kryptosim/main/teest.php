854ccc01f28136136f4ef7e11f295827d0b7613c
ToasterXE <114070865+ToasterXE@users.noreply.github.com>
2023-12-12 21:18:59 +0100

    eeeeeeeee

---
+++ kryptosim/main/teest.php
@@ -0,0 +1,18 @@
+<?php
+$password = "$2y$10$.g1iYEEjH3UVCLOwAGjb1.v9uHKGk1GW4BzADW60ZCrjQgYRyQiZO$";
+$e = "eeeeeee";
+
+function generateRandomString($length) {
+    return substr(str_shuffle(str_repeat($x='abcdefghijklmnopqrstuvwxyz', ceil($length/strlen($x)) )),1,$length);
+}
+
+
+
+while(!password_verify($e, $password)){
+    $e = generateRandomString(7);
+}
+echo ($e);
+?>
+<html>
+    e
+</html>
